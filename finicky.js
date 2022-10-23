1d0ddd3f5a6e9d91e297f1386dfd86cf790939c4
Andrei Lineischikov <code@lineischikov.icu>
2022-10-23 13:04:06 +0300

    Update

--- finicky.js
+++ finicky.js
@@ -1,42 +1,130 @@
-// Default = semrush
-// Profile 1 = personal
+function slackRewrite({ url, urlString }) {
+  const subdomain = url.host.slice(0, -10);
+  const pathParts = url.pathname.split("/");
+
+  let team,
+    patterns = {};
+  if (subdomain != "app") {
+    switch (subdomain) {
+      case "<teamname>":
+      case "<corpname>.enterprise":
+        team = "T00000000";
+        break;
+      default:
+        finicky.notify(
+          `No Slack team ID found for ${url.host}`,
+          `Add the team ID to ~/.finicky.js to allow direct linking to Slack.`
+        );
+        return url;
+    }
+
+    if (subdomain.slice(-11) == ".enterprise") {
+      patterns = {
+        file: [/\/files\/\w+\/(?<id>\w+)/],
+      };
+    } else {
+      patterns = {
+        file: [/\/messages\/\w+\/files\/(?<id>\w+)/],
+        team: [/(?:\/messages\/\w+)?\/team\/(?<id>\w+)/],
+        channel: [/\/(?:messages|archives)\/(?<id>\w+)(?:\/(?<message>p\d+))?/],
+      };
+    }
+  } else {
+    patterns = {
+      file: [
+        /\/client\/(?<team>\w+)\/\w+\/files\/(?<id>\w+)/,
+        /\/docs\/(?<team>\w+)\/(?<id>\w+)/,
+      ],
+      team: [/\/client\/(?<team>\w+)\/\w+\/user_profile\/(?<id>\w+)/],
+      channel: [/\/client\/(?<team>\w+)\/(?<id>\w+)(?:\/(?<message>[\d.]+))?/],
+    };
+  }
+
+  for (let [host, host_patterns] of Object.entries(patterns)) {
+    for (let pattern of host_patterns) {
+      let match = pattern.exec(url.pathname);
+      if (match) {
+        let search = `team=${team || match.groups.team}`;
+
+        if (match.groups.id) {
+          search += `&id=${match.groups.id}`;
+        }
+
+        if (match.groups.message) {
+          let message = match.groups.message;
+          if (message.charAt(0) == "p") {
+            message = message.slice(1, 11) + "." + message.slice(11);
+          }
+          search += `&message=${message}`;
+        }
+
+        let output = {
+          protocol: "slack",
+          username: "",
+          password: "",
+          host: host,
+          port: null,
+          pathname: "",
+          search: search,
+          hash: "",
+        };
+        let outputStr = `${output.protocol}://${output.host}?${output.search}`;
+        finicky.log(`Rewrote Slack URL ${urlString} to deep link ${outputStr}`);
+        return output;
+      }
+    }
+  }
+
+  return url;
+}
 
 module.exports = {
-    options: {
-        // Hide the finicky icon from the top bar. Default: false
-        hideIcon: true
+  options: {
+    hideIcon: true,
+  },
+  defaultBrowser: "safari",
+  handlers: [
+    {
+      match: [
+        "*.google.com/*",
+        "*.crowdin.com/*",
+        "*.recruitee.com/*",
+        "*.miro.com/*",
+        /localhost/,
+        /yougood/,
+        /semrush/,
+        "*.bamboohr.com/*",
+      ],
+      browser: {
+        name: "Brave Browser",
+        profile: "Default", // Work
+      },
     },
-    defaultBrowser: 'safari',
-    // defaultBrowser: {
-    //     name: "Brave Browser",
-    //     profile: "Profile 1", // Personal
-    // },
-    rewrite: [
-        {
-            // Redirect all urls to use https
-            match: ({ url }) => url.protocol === "http",
-            url: { protocol: "https" }
-        }
-    ],
-    handlers: [
-        {
-            match: [
-                /^https:\/\/mail\.google\.com\/.*$/,
-                /^https:\/\/meet\.google\.com\/.*$/,
-                /^https:\/\/docs\.google\.com\/.*$/,
-                /^https:\/\/drive\.google\.com\/.*$/,
-                /^https:\/\/crowdin\.com\/.*$/,
-                /^https:\/\/.*recruitee\.com\/.*$/,
-                /sendgrid/,
-                /zoom.us/,
-                /figma/,
-                /yougood/,
-                /semrush/
-            ],
-            browser: {
-                name: "Brave Browser",
-                profile: "Default", // Work
-            }
-        },
-    ]
-}
+    {
+      match: ({ url }) => url.protocol === "slack",
+      browser: "/Applications/Slack.app",
+    },
+    {
+      match: [
+        "zoom.us/j/*",
+        finicky.matchDomains(/.*\zoom.us\/j/),
+        /zoom.us\/j\//,
+      ],
+      browser: "us.zoom.xos",
+    },
+    {
+      match: "https://www.figma.com/file/*",
+      browser: "Figma",
+    },
+  ],
+  rewrite: [
+    {
+      match: ["*.slack.com/*"],
+      url: slackRewrite,
+    },
+    {
+      match: finicky.matchDomains(["google.com"]),
+      url: "https://duckduckgo.com",
+    },
+  ],
+};
